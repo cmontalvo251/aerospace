11a28ba0bbd0e9ac30179cf7bf8478529e2335e3
Carlos Montalvo T540p <cmontalvo251@gmail.com>
2022-02-24 11:01:14 -0600

    edited inertia calcs

--- gnc_qualification/matlab/inertia.m
+++ gnc_qualification/matlab/inertia.m
@@ -1,4 +1,4 @@
-function [Inertia,max_moment_arm,max_area] = inertia(L,W,D,total_mass,mass_solar_panel,number_of_panels,LWD_sp,xyz_sp)
+function [Inertia,max_moment_arm,max_area] = inertia(Lx,Wy,Dz,total_mass,mass_solar_panel,number_of_panels,LWD_sp,xyz_sp)
 %%This code will compute the inertia of ta CubeSAT with Solar Panels
 % L is the length of the satellite in cm
 % W is the width of the satellite in cm
@@ -9,26 +9,39 @@ function [Inertia,max_moment_arm,max_area] = inertia(L,W,D,total_mass,mass_solar
 % LWD_sp is the size of 1 panel in meters
 % xyz_sp is the position of the solar panels from the center of mass of the cuboid in meters
 
-max_moment_arm = max([L,W,D]/200);
+max_moment_arm = max([Lx,Wy,Dz]/2);
 disp(['Maximum Moment Arm (m) = ',num2str(max_moment_arm)])
-area1 = L*W/(100^2);
-area2 = W*D/(100^2);
-area3 = L*D/(100^2);
+area1 = Lx*Wy; %%%Top face
+area2 = Wy*Dz; %%Ram face
+area3 = Lx*Dz; %%%Side face
 max_area = max([area1,area2,area3]);
-disp(['Maximum Area (m^2) = ',num2str(max_area)])
+disp(['Maximum Area (No Panels) (m^2) = ',num2str(max_area)])
 
 %%%Mass/Inertia of Cuboid
 mass_cuboid = total_mass - mass_solar_panel*number_of_panels;
-Ixx = (mass_cuboid/12)*(L^2+W^2)/(100^2);
-Iyy = (mass_cuboid/12)*(L^2+D^2)/(100^2);
-Izz = (mass_cuboid/12)*(W^2+D^2)/(100^2);
+Ixx = (mass_cuboid/12)*(Wy^2+Dz^2);
+Iyy = (mass_cuboid/12)*(Lx^2+Dz^2);
+Izz = (mass_cuboid/12)*(Lx^2+Wy^2);
 Inertia_cuboid = [[Ixx,0,0];[0,Iyy,0];[0,0,Izz]];
 
 %%%Inertia of Solar panel about centroid
-Length_sp = LWD_sp(1);
-Width_sp = LWD_sp(2);
-Depth_sp = LWD_sp(3);
-Inertia_sp_centroid = mass_solar_panel/12.0 * [[Length_sp^2 + Width_sp^2,0,0];[0,Length_sp^2+Depth_sp^2,0];[0,0,Width_sp^2+Depth_sp^2]];
+Lx_sp = LWD_sp(1,1);
+Wy_sp = LWD_sp(2,1);
+Dz_sp = LWD_sp(3,1);
+Inertia_sp_centroid = mass_solar_panel/12.0 * [[Wy_sp^2 + Dz_sp^2,0,0];[0,Lx_sp^2+Dz_sp^2,0];[0,0,Lx_sp^2+Wy_sp^2]];
+
+%%%Compute Max Area of Solar Panels
+area1sp = 0; %%Top face (basically nothing)
+area2sp = (Wy_sp * Dz_sp)*number_of_panels;  %%%Ram face
+area3sp = 0; %%side face (basically nothing)
+
+%%%Compute Area of Faces with panels
+areaT1 = area1 + area1sp;
+areaT2 = area2 + area2sp;
+areaT3 = area3 + area3sp;
+
+max_areaT = max([areaT1,areaT2,areaT3]);
+disp(['Maximum Area (with panels) (m^2) = ',num2str(max_areaT)])
 
 %disp(['Inertia of Cuboid (kg-m^2) = '])
 %disp(Inertia_cuboid)
@@ -38,9 +51,9 @@ Inertia_sp_centroid = mass_solar_panel/12.0 * [[Length_sp^2 + Width_sp^2,0,0];[0
 %##Use the parallel axis theorem and move the sp to the centroid of the cuboid
 Inertia = Inertia_cuboid;
 for i = 1:number_of_panels
-	lx = xyz_sp(i,1);
-	ly = xyz_sp(i,2);
-	lz = xyz_sp(i,3);
+	lx = xyz_sp(1,i);
+	ly = xyz_sp(2,i);
+	lz = xyz_sp(3,i);
 	rsp = [[0,-lz,ly];[lz,0,-lx];[-ly,lx,0]];
 	rspT = rsp';
 	Inertia_sp = Inertia_sp_centroid + mass_solar_panel*rsp*rspT;
