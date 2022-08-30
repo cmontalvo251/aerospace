d2b4f291857f4537fa68d5385de7389c9d21d08f
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-08-29 20:35:42 -0500

    Working on Ground track for CubeSATs

---
+++ satcomms/gpscomms.py
@@ -0,0 +1,243 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import sys
+import os
+
+##In order to import this toolbox into a python script you need to 
+##do the following. Copy the following lines of code below
+# import sys
+# sys.path.append('/home/carlos/Dropbox/BlackBox/plotting')
+# from gps import *
+
+# or 
+# In order to get python to search for all of your lovely blackbox 
+# python routines. Add this to your .bashrc file
+
+# for d in /home/carlos/Dropbox/BlackBox/*/; do
+# 	PYTHONPATH+=:$d
+# done
+# export PYTHONPATH
+
+# In order to get this to work in Thonny you need to navigate to
+# /home/user/.thonny/BundledPython36/lib/python3.6/site-packages and place
+# a symbolic link here
+
+# In Enthough you need to make symbolic links here
+# /home/carlos/.local/share/canopy/edm/envs/User/lib/python2.7/site-packages
+
+class GPSCOMMS():
+    def __init__(self):
+        self.NM2FT=6076.115485560000
+        self.FT2M=0.3048
+        self.G = 6.6742*10**-11; #%%Gravitational constant
+        self.GPSVAL = 60.0*self.NM2FT*self.FT2M
+        self.EarthRadius_ae = 6378140. ##meters mean eq radius
+        self.MEarth = 5.97219e24 #kg
+        self.muEarth = self.G*self.MEarth
+        #23 hours 56 minutes 4 seconds
+        self.solar_day_sec = 23*3600.0 + 56*60.0 + 4.0
+        self.wEarth = 2*np.pi/self.solar_day_sec
+        self.elements = False
+
+    def NMEA_LAT_LON(self,IN):
+        l = np.float(IN)/100.0;
+        l_deg = np.floor(l);
+        l_min = (l-l_deg)*100.0;
+        out = l_deg + l_min/60.0;
+        return out
+
+    def NMEA_TIME(self,time_raw,units):
+        #try splitting by :
+        times = time_raw.split(':')
+        if len(times) == 1:
+            #split didnt work which means there are no colons
+            #print time_raw
+            hour = np.float(time_raw[0:2])
+            minute = np.float(time_raw[2:4])
+            sec = np.float(time_raw[4:6])
+            msec = 0
+            #print hour,minute,sec
+        else:
+            hour = np.float(times[0])
+            minute = np.float(times[1])
+            sec = np.float(times[2])
+            try:
+                msec = np.float(times[3])
+            except:
+                msec = 0;
+        time = 0
+        if units == 'sec':
+            time = msec/1000 + sec + minute*60 + hour*3600
+        elif units == 'hrs':
+            time = hour + minute/60 + sec/3600 + (msec/1000)/3600
+        else:
+            print('Invalid Unit in NMEA_TIME. Returning 0')
+        #print time
+        return time
+
+    def computeGeocentricLATLON(self,x,y,z,t):
+        xprime = np.sqrt(x**2 + y**2)
+        zprime = z
+        latitude = np.arctan2(zprime,xprime)*180/np.pi
+        longitude = np.asarray(np.arctan2(y,x)*180.0/np.pi)-180.0/np.pi*wEarth*t
+        #longitude[longitude<0]+=360.0
+        #norm = np.sqrt(x**2 + y**2 + z**2)
+        #phi = np.arccos(self.zsat / rho)
+        #the = np.arctan2(self.ysat,self.xsat);
+        #self.latitude = 90 - phi*(180 /np.pi);
+        #self.longitude = the*(180/np.pi);
+        return latitude,longitude
+
+    def IFOV(self,swath,deltas,az_rad):
+        cosdtprime = np.cos(swath)*np.sin(deltas)+np.sin(swath)*np.cos(deltas)*np.cos(az_rad)
+        dtprime = np.arccos(cosdtprime)
+        dt = 90.0 - dtprime * 180.0/np.pi
+        dt_rad = dt*np.pi/180.0
+        cosdL = np.asarray((np.cos(swath)-np.sin(deltas)*np.sin(dt_rad))/(np.cos(deltas)*np.cos(dt_rad)))
+        cosdL[abs(cosdL)>1.0] = 1.0
+        dL = np.arccos(cosdL)*180/np.pi
+        return dt,dL
+
+    def LATLON2Cartesian(self,lat,lon,alt):
+        z = alt*np.sin(lat*np.pi/180.0)
+        xyprime = alt*np.cos(lat*np.pi/180.0)
+        x = xyprime*np.cos(lon*np.pi/180.0)
+        y = xyprime*np.sin(lon*np.pi/180.0)
+        return x,y,z
+
+    def convertXY2LATLON(self,xy,origin):
+        x = xy[0]
+        y = xy[1]
+        Ox = origin[0]
+        Oy = origin[1]
+        lat = x/self.GPSVAL + Ox
+        lon = y/(self.GPSVAL*np.cos(Ox*np.pi/180)) + Oy
+        return np.asarray([lat,lon])
+
+    def convertLATLON(self,lat_lon,origin):
+        lat = lat_lon[0]
+        lon = lat_lon[1]
+        Ox = origin[0]
+        Oy = origin[1]
+        x = (lat-Ox)*self.GPSVAL
+        y = (lon-Oy)*self.GPSVAL*np.cos(Ox*np.pi/180)
+        return np.asarray([x,y])
+
+    def plotEARTH(self,ax):
+        u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+        xsph = np.cos(u)*np.sin(v)*self.EarthRadius_ae
+        ysph = np.sin(u)*np.sin(v)*self.EarthRadius_ae
+        zsph = np.cos(v)*self.EarthRadius_ae
+        ax.plot_wireframe(xsph,ysph,zsph,color='blue')
+        return
+
+    def computeOrbitalElements(self,height_at_perogee_km,ECC,INC,LAN,ARG):
+        if self.elements == False:
+            ##PEROGEE IS GIVEN
+            self.rp = height_at_perogee_km*1000 + self.EarthRadius_ae
+            #SOLVE FOR APOGEE
+            ##ECC = (ra - rp)/(ra + rp)
+            self.ra = self.rp*(1+ECC)/(1-ECC)
+            ##SEMI MAJOR AXIS
+            self.a = (self.ra + self.rp)/2.0
+            #inclination in radians
+            self.i = INC*np.pi/180.0
+            ###Longitude of the Ascending Node in radians
+            self.W = LAN*np.pi/180.0
+            #Argument of the periaps in radians
+            self.w = ARG*np.pi/180.0
+            ###The parameter
+            self.p = self.a*(1-ECC**2)
+            ##Coefficient of velocity
+            self.coeff = np.sqrt(self.muEarth/self.p)
+            self.elements = True
+            ###Rotation matrix to Earth Centered Inertial Frame (ECI)
+            self.TPI = np.asarray([[np.cos(self.W)*np.cos(self.w)-np.sin(self.W)*np.sin(self.w)*np.cos(self.i),-np.cos(self.W)*np.sin(self.w)-np.sin(self.W)*np.cos(self.w)*np.cos(self.i),np.sin(self.W)*np.sin(self.i)],
+                 [np.sin(self.W)*np.cos(self.w)+np.cos(self.W)*np.sin(self.w)*np.cos(self.i),-np.sin(self.W)*np.sin(self.w)+np.cos(self.W)*np.cos(self.w)*np.cos(self.i),-np.cos(self.W)*np.sin(self.i)],
+                 [np.sin(self.w)*np.sin(self.i),np.cos(self.w)*np.sin(self.i),np.cos(self.i)]])
+        return
+        
+    def getStateVector(self,height_at_perogee_km,ECC,INC,LAN,ARG,nu):
+        ##First compute the orbital elements if needed
+        self.computeOrbitalElements(height_at_perogee_km,ECC,INC,LAN,ARG)
+
+        #Then we compute the following:
+        ##Radius
+        r = self.p/(1+ECC*np.cos(nu))
+        ##Orbital plane x and y
+        xp = r*np.cos(nu)
+        yq = r*np.sin(nu)
+        zr = 0*xp
+        ##Velocity
+        up = self.coeff*(-np.sin(nu))
+        vq = self.coeff*(ECC+np.cos(nu))
+        wr = 0*up
+        #Rotate to ECI frame
+        xyzO = np.asarray([xp,yq,zr]) ##3x1 vector
+        uvwO = np.asarray([up,vq,wr])
+        xyzi = np.matmul((self.TPI),xyzO)
+        uvwi = np.matmul((self.TPI),uvwO)
+        xi = xyzi[0]
+        yj = xyzi[1]
+        zk = xyzi[2]
+        ui = uvwi[0]
+        vj = uvwi[1]
+        wk = uvwi[2]
+        
+        return xi,yj,zk,ui,vj,wk
+
+    def kepler_orbit(self,height_at_perogee_km,ECC,INC,LAN,ARG):
+        ##First compute the orbital elements if needed
+        self.computeOrbitalElements(height_at_perogee_km,ECC,INC,LAN,ARG)
+
+        ##True Anamoly
+        nu = np.linspace(0,2*np.pi,100)
+
+        #Then run through the true anomaly vector and get the state vector
+        xi = 0*nu
+        yj = 0*nu
+        zk = 0*nu
+        ui = 0*nu
+        vj = 0*nu
+        wk = 0*nu
+        for x in range(0,len(xi)):
+            xii,yjj,zkk,uii,vjj,wkk = self.getStateVector(height_at_perogee_km,ECC,INC,LAN,ARG,nu[x])
+            xi[x] = xii
+            yj[x] = yjj
+            zk[x] = zkk
+            ui[x] = uii
+            vj[x] = vjj
+            wk[x] = wkk
+        return xi,yj,zk,ui,vj,wk,nu*180.0/np.pi
+
+
+    def HHMM_Format(self,time_vec,del_min):
+        ##Assume this time_vec is in format HH.(HH/60)
+        time_label = []
+        last_time = time_vec[0]-del_min
+        ctr = -1
+        for time in time_vec:
+            hour = int(np.floor(time))
+            minute = int(np.round((time-hour)*60))
+            #print time,hour,minute
+            str_minute = str(minute)
+            if (minute < 10):
+                str_minute = '0' + str_minute
+            ##Check and make sure enough time has passed
+            str_time = str(hour)+':'+str_minute
+            if (time-last_time)*60.0 >= del_min-1e-2:
+                last_time = time
+                #print str_time,ctr
+                ctr+=1
+                time_label.append(str_time)
+
+        #print time_label
+        xticks = np.linspace(time_vec[0],time_vec[-1],ctr+1)
+
+        return time_label,xticks
+
+# Copyright - Carlos Montalvo 2016
+# You may freely distribute this file but please keep my name in here
+# as the original owner
---
+++ satcomms/ground_track.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
+import sys
+from gpscomms import GPSCOMMS
+
+##Make the GPS class
+GPS = GPSCOMMS()
+
+###CREATE AN ORBIT AROUND THE EARTH
+height_at_perigee_km = 6000.0 #in kilometers
+ECC = 0.3 #0 is circular orbit
+INC = 56.0 #degrees
+LAN = 30.0 #Longitude of the ascending node in degrees
+ARG = 30. #argument of the periaps in degrees
+
+##USING THE ORBITAL ELEMENTS YOU CAN GET ANALYTIC ORBITS JUST USING KEPLERS EQUATIONS
+xsat_a,ysat_a,zsat_a,xdot_a,ydot_a,zdot_a,nu_deg = GPS.kepler_orbit(height_at_perigee_km,ECC,INC,LAN,ARG)
+
+##The problem with Keplerian orbits is we don't have the time component. In order to do that we
+##Need to get an initial state vector and then integrate the equations of motion
+#The trailing 0 just means give the coordinate at a true anomaly of 0
+x0,y0,z0,u0,v0,w0 = GPS.getStateVector(height_at_perigee_km,ECC,INC,LAN,ARG,0)
+
+###PLOT 3D ORBIT
+fig = plt.figure('3-D')
+ax = fig.add_subplot(111,projection='3d')
+ax.plot(xsat_a,ysat_a,zsat_a,color = 'red', linestyle = 'solid')
+ax.plot(xsat_a[0:1],ysat_a[0:1],zsat_a[0:1],'g*',markersize = 20)
+plt.title('Satellite Orbit')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+GPS.plotEARTH(ax)
+
+##PLOT VELOCITY
+fig = plt.figure()
+plt.plot(nu_deg,xdot_a,label='X')
+plt.plot(nu_deg,ydot_a,label='Y')
+plt.plot(nu_deg,zdot_a,label='Z')
+plt.grid()
+plt.legend()
+plt.xlabel('True Anamoly (deg)')
+plt.ylabel('Velocity (m/s)')
+
+plt.show()
+sys.exit()
+
+#tsat = data_sat[:,0] #time
+
+
+##Compute Distance from centroid of Earth
+norm = np.sqrt(xsat**2 + ysat**2 + zsat**2) 
+
+##Compute Latitude Longitude and (Geocentric)
+print('Computing Geocentric Lat/Lon')
+latitude,longitude = GPS.computeGeocentricLATLON(xsat,ysat,zsat,0*tsat)
+#Convert to Radians because degrees suck
+longitude_rad = longitude*np.pi/180.0
+latitude_rad = latitude*np.pi/180.0
+
+###Lat/Long (Geodetic)
+
+
+###Swath Angle
+print('Computing Swath Angle')
+observer_angle_deg = 0.0 ##this will be higher if you can't see the horizon (this is in degrees)
+sinrho = GPS.EarthRadius_ae/norm
+sineta = np.cos(observer_angle_deg*np.pi/180.0)*sinrho
+eta_rad = np.arcsin(sineta)
+swath_angle_deg = 90.0 - eta_rad*180.0/np.pi - observer_angle_deg
+swath_angle_rad = swath_angle_deg*np.pi/180.0
+
+###Compute the Swath Vectors
+yscalar = norm*np.tan(swath_angle_deg*np.pi/180.0)
+latitudesw1 = 0*latitude
+latitudesw2 = 0*latitude
+longitudesw1 = 0*latitude
+longitudesw2 = 0*latitude
+print('Computing Swath Vectors and Swath Lat/Lons')
+for n in range(0,len(xsat)):
+    xyz = np.asarray([xsat[n],ysat[n],zsat[n]])
+    vxyz = np.asarray([xdot[n],ydot[n],zdot[n]])
+    hn = np.cross(xyz,vxyz)
+    #h[:,n] = hn
+    yn = yscalar[n]*hn/np.linalg.norm(hn)
+    sw1 = xyz + yn
+    sw2 = xyz - yn
+    lat1,long1 = GPS.computeGeocentricLATLON(sw1[0],sw1[1],sw1[2],0*tsat[n]) #0 is here so that gps.py does not turn the Earth
+    lat2,long2 = GPS.computeGeocentricLATLON(sw2[0],sw2[1],sw2[2],0*tsat[n]) #we will be turning the Earth in MultiSAT++
+    latitudesw1[n] = lat1
+    latitudesw2[n] = lat2
+    longitudesw1[n] = long1
+    longitudesw2[n] = long2
+print('Done')
+
+##Plot longitude vs time
+#plt.figure()
+#plt.plot(tsat,longitude)
+#plt.show()
+#sys.exit()
+
+###Plot Ground Track
+plt.figure()
+n = 0
+az_deg = np.linspace(0,180,100)
+az_rad = az_deg * np.pi/180.0
+
+nframes = 1000.0
+skip = int(len(xsat)/nframes)
+
+##Location of Ground Station
+#KMOB
+#lonG = -88.175228
+#latG = 30.691615
+#MOB Shifted
+latG = 30.691615
+lonG = 25.0
+#Origin
+#lonG = 0.0
+#latG = 0.0
+#Random
+#lonG = 100.0
+#latG = 40.0
+
+INSIDE = 0
+if skip < 0:
+	skip = 1
+if skip > len(xsat):
+	skip = len(xsat)
+
+startTime = []
+endTime = []
+deltaTime = []
+
+for ifov in range(0,len(xsat),skip):
+	#Clear the axis
+	plt.cla()
+
+	##Plot Ground Track and Swatch Track
+	plt.plot(longitude,latitude)
+	plt.plot(longitudesw1,latitudesw1,'r-')
+	plt.plot(longitudesw2,latitudesw2,'r-')
+
+	##Plot Current Position
+	plt.plot(longitude[ifov],latitude[ifov],'b*',markerSize=20)
+
+	##Plot Location of Ground Station
+	plt.plot(lonG,latG,'rs',markerSize=20)
+
+	##Plot line from ground station to satellite
+	plt.plot([longitude[ifov],lonG],[latitude[ifov],latG],'y-')
+
+	##Compute the IFOV
+	deltas = latitude_rad[ifov]
+	deltas_deg = latitude[ifov]
+	Ls = longitude_rad[ifov]
+	Ls_deg = longitude[ifov]
+	swath = swath_angle_rad[ifov]
+	dt,dL = GPS.IFOV(swath,deltas,az_rad)
+	dt_all = np.hstack((dt,dt[-1::-1]))
+	Lt_all = np.hstack((Ls_deg-dL,Ls_deg+dL[-1::-1]))
+	
+	##Compute Azimuth to GND
+	dlong_deg = lonG - Ls_deg
+	dlat_deg  = latG - deltas_deg
+	dlong_rad = dlong_deg*np.pi/180.0
+	dlat_rad = dlat_deg*np.pi/180.0
+	azG_rad = np.arctan2(dlong_rad,dlat_rad)
+	azG_deg = azG_rad*180.0/np.pi
+
+	##Compute the limit of lat/lon in that direction
+	latFOV_G,dLG = GPS.IFOV(swath,deltas,azG_rad)
+	longFOV_G1 = longitude[ifov] + dLG
+	longFOV_G2 = longitude[ifov] - dLG
+
+	###Compute the norm from sat to ground station
+	r = GPS.EarthRadius_ae
+
+	##Compute norm assuming flat earth with satellite as origin
+	xG,yG,zG = GPS.LATLON2Cartesian(latG,lonG,r)
+	xs,ys,zs = GPS.LATLON2Cartesian(latitude[ifov],longitude[ifov],r)
+	sat2G = np.sqrt((xG-xs)**2 + (yG-ys)**2+(zG-zs)**2)
+	###Compute norm from sat to IFOV
+	xFOV1,yFOV1,zFOV1 = GPS.LATLON2Cartesian(latFOV_G,longFOV_G1,r)
+	sat2FOV1 = np.sqrt((xs-xFOV1)**2 + (ys-yFOV1)**2+(zs-zFOV1)**2)
+	xFOV2,yFOV2,zFOV2 = GPS.LATLON2Cartesian(latFOV_G,longFOV_G2,r)
+	sat2FOV2 = np.sqrt((xs-xFOV2)**2 + (ys-yFOV2)**2+(zs-zFOV2)**2)
+
+	##Plot the IFOV in black or green
+	if sat2G < sat2FOV1 or sat2G < sat2FOV2:
+		plt.plot(Lt_all,dt_all,'g-')
+		plt.plot(longFOV_G1,latFOV_G,'gs',markerSize=20)
+		plt.plot(longFOV_G2,latFOV_G,'gs',markerSize=20)
+		"""
+		print('g',xG,yG,zG)
+		print('s',xs,ys,zs)
+		print('fov1',xFOV1,yFOV1,zFOV1)
+		print('lat/lon',latitude[ifov],longitude[ifov])
+		print('lat/lonG',latG,longG)
+		print('lat/lonFOV',latFOV_G,longFOV_G1)
+		print('sat2G',sat2G/1000.0)
+		print('sat2FOV1',sat2FOV1/1000.0)
+		"""
+		if INSIDE == 0:
+			tstart = tsat[ifov]
+			startTime.append(tstart)
+			print('IN VIEW = ',tstart)
+		INSIDE = 1
+	else:
+		plt.plot(Lt_all,dt_all,'k-')
+		plt.plot(longFOV_G1,latFOV_G,'ks',markerSize=20)
+		plt.plot(longFOV_G2,latFOV_G,'ks',markerSize=20)
+		if INSIDE == 1:
+			tend = tsat[ifov]
+			endTime.append(tend)
+			deltaTime.append((tend - tstart)/60.0)
+			print('OUT OF VIEW = ',tend)
+			print('Delta Time (min) = ',(tend - tstart)/60.0)
+			
+		INSIDE = 0
+
+	
+	
+	plt.xlabel('Longitude (Deg)')
+	plt.ylabel('Latitude (Deg)')
+	plt.title(str(tsat[ifov]))
+	plt.grid()
+	plt.pause(0.1)
+
+
+
