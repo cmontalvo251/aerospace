36a2f9cdfb70208c988f243c756bd17d3c8c0b92
Carlos Montalvo <cmontalvo251@gmail.com>
2022-10-14 11:28:12 -0500

    Finished lets go to a satellite code

--- ksp/lets_go_to_a_satellite.py
+++ ksp/lets_go_to_a_satellite.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 ####CONSTANTS
 
 ###MILY WAY GALAXY
-'''
 Radius_of_Planet = 6379000 #meters of Planet
 Altitude_of_Parking_Orbit_Planet = 200000 #meters
 G = 6.67e-11 #SI
@@ -13,18 +12,18 @@ Radius_of_Satellite_from_Center_of_Satellite = 1738100 #meters of Satellite we i
 Distance_bt_Planet_Center_2_Satellite_Center = 384467000 #meters (bt = between)
 mSatellite = 0.07346e24
 Altitude_of_Parking_Orbit_Satellite = 112000 #meter - This is what Apollo
-'''
 
 ###KERMS SOLAR SYSTEM - Kerbin to Mun
+"""
 Radius_of_Planet = 600000 #meters of Planet
 Altitude_of_Parking_Orbit_Planet = 80000 #meters
 G = 6.67e-11 #SI
 mPlanet = 5.291e22 #kg
-Radius_of_Satellite_from_Center_of_Satellite = 20000 #meters of Satellite we intend to travel to (Moon, Mun, Minmus...)
+Radius_of_Satellite_from_Center_of_Satellite = 200000 #meters of Satellite we intend to travel to (Moon, Mun, Minmus...)
 Distance_bt_Planet_Center_2_Satellite_Center = 12000000 #meters (bt = between)
 mSatellite = 9.759e20
 Altitude_of_Parking_Orbit_Satellite = 5000 #meter - This is what Apollo
-
+"""
 
 ###COMPUTATION
 
@@ -52,7 +51,7 @@ print('Velocity of Parking Orbit around Planet = ',v_parking_orbit_planet,' m/s'
 xParkingPlanet = r*np.cos(angle)
 yParkingPlanet = r*np.sin(angle)
 ###DELTAV TO GET TO PARKING ORBIT AROUND PLANET
-DV1 = 2*v_parking_orbit_planet
+DV1 = 1.5*v_parking_orbit_planet
 print('DV of First Manuever (Surface to Parking Orbit) = ',DV1,' m/s')
 ###COMPUTE OUR TRANSFER ORBIT
 ra = Distance_bt_Planet_Center_2_Satellite_Center + Radius_of_Satellite_from_Center_of_Satellite + Altitude_of_Parking_Orbit_Satellite
@@ -72,6 +71,39 @@ rTIO = pTIO/(1+eTIO*np.cos(angle))
 xTIO = rTIO*np.cos(angle)
 yTIO = rTIO*np.sin(angle)
 
+##Compute Velocity of Satellite
+aSat = Distance_bt_Planet_Center_2_Satellite_Center
+eSat = 0
+pSat = aSat
+hSat = np.sqrt(pSat*muPlanet)
+rSat = aSat
+v_satellite = hSat/rSat
+print("Velocity of Satellite = ",v_satellite," m/s")
+
+##Compute Velocity Relative to Satellite
+v_spacecraft_relative_2_satellite = np.abs(v_satellite-va)
+print("Velocity of Spacecraft Relative to Satellite = ",v_spacecraft_relative_2_satellite," m/s")
+
+###Compute the Parking Orbit Around the Satellite
+aParkingSat = Radius_of_Satellite_from_Center_of_Satellite + Altitude_of_Parking_Orbit_Satellite
+eParkingSat = 0
+pParkingSat = aParkingSat
+hParkingSat = np.sqrt(pParkingSat*muSatellite)
+rParkingSat = aParkingSat
+v_parking_satellite = hParkingSat/rParkingSat
+print("Velocity of Spacecraft in Orbit Around Satellite = ",v_parking_satellite," m/s")
+DV3 = v_parking_satellite - v_spacecraft_relative_2_satellite
+print("Third DV Manuever to Orbit the Satellite = ",DV3," m/s")
+xParkingSat = aParkingSat*np.cos(angle) - Distance_bt_Planet_Center_2_Satellite_Center
+yParkingSat = aParkingSat*np.sin(angle)
+
+###Land
+DV4 = v_parking_satellite ##Landing
+DV5 = DV4 #Takeoff
+DV6 = DV2
+print("DV4 5 and 6 = ",DV4,DV5,DV6," m/s")
+print("Total DV = ",DV1 + DV2 + DV3 + DV4 + DV5 + DV6)
+
 ###PLOTS
 
 ###OVERALL PLOT
@@ -81,6 +113,7 @@ plt.plot(xParkingPlanet,yParkingPlanet,'r-')
 plt.plot(xSatellite,ySatellite,'k-')
 plt.plot(xSatellite_Orbit,ySatellite_Orbit,'k-')
 plt.plot(xTIO,yTIO,'g-')
+plt.plot(xParkingSat,yParkingSat,'m-')
 plt.axis('equal')
 plt.grid()
 
@@ -91,6 +124,7 @@ plt.plot(xParkingPlanet,yParkingPlanet,'r-')
 plt.plot(xSatellite,ySatellite,'k-')
 plt.plot(xSatellite_Orbit,ySatellite_Orbit,'k-')
 plt.plot(xTIO,yTIO,'g-')
+plt.plot(xParkingSat,yParkingSat,'m-')
 plt.xlim([-1.5*Radius_of_Planet,1.5*Radius_of_Planet])
 plt.ylim([-1.5*Radius_of_Planet,1.5*Radius_of_Planet])
 plt.grid()
@@ -102,6 +136,7 @@ plt.plot(xParkingPlanet,yParkingPlanet,'r-')
 plt.plot(xSatellite,ySatellite,'k-')
 plt.plot(xSatellite_Orbit,ySatellite_Orbit,'k-')
 plt.plot(xTIO,yTIO,'g-')
+plt.plot(xParkingSat,yParkingSat,'m-')
 var = Distance_bt_Planet_Center_2_Satellite_Center
 rvar = Radius_of_Satellite_from_Center_of_Satellite
 plt.xlim([-var-1.5*rvar,-var+1.5*rvar])
