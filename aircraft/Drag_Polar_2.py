a930a2979c484c5b4f309547a23f0a88145f1995
Carlos Montalvo <15804814+cmontalvo251@users.noreply.github.com>
2023-02-24 10:04:13 -0600

    Update Drag_Polar_2.py
    edited comments and some typos

--- aircraft/Drag_Polar_2.py
+++ aircraft/Drag_Polar_2.py
@@ -1,17 +1,17 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-a0 = 4.0
-a = np.linspace(-4,12,1000)
-Cd0 = 0.025
+a0 = 0.0 #angle of attack at minimum drag
+a = np.linspace(-6,6,1000) #range for your drag polar
+Cd0 = 0.01 #drag coefficient at minimum drag
 
 #Cd = Cd0 + Cda*(a-a0)^2
 ##Grab a data point
-astar = 12
-Cdstar = 0.04
-Cda = (Cd-Cd0)/(astar-a0)**2
-print('Cda = ',Cda)
+astar = 6 
+Cdstar = 0.02
+Cda = (Cdstar-Cd0)/(astar-a0)**2
+print('Cda /deg**2 = ',Cda)
+print('Cda /rad**2 = ',Cda*(180./np.pi)**2)
 Cd = Cd0 + Cda*(a-a0)**2
 
 plt.plot(a,Cd)
